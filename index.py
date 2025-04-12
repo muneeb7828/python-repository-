9011fb9f6fcde9aa10636a96a1d60303313fdd9b
Muneeb ur rehman <urrehmanm51@gmail.com>
2025-04-12 15:57:31 +0530

    This is a repository of python

---
+++ index.py
@@ -0,0 +1,115 @@
+var1="muneeb"
+print(var1)
+
+# jese java script me datatypes hote he vese hi isme class hote he
+
+print(type(var1))   # class 'str'
+
+print("m" in var1)   # memberhsip operator
+
+num=20
+print(type(num))  # class 'int'
+
+num2=12.20
+print(type(num2)) # class 'float'
+
+name="muneeb"
+array=["hellow","my","name","is",name,24]
+print(array)
+print(type(array)) # class 'List'
+
+obj={"name":"muneeb"}
+print(type(obj))  # class 'dict'
+
+Fname="Muneeb"
+Lname="ur rehman"
+
+
+obj2={Fname,Lname,24}
+print(type(obj2))   # class 'set'
+
+
+variale="muneeb","ur","rehman","age",24
+print(type(variale))  # class 'tuple'
+
+
+
+# var4=input("enter your name")  # ye prompt ke liye hota he
+# print(type(var4))
+
+
+# var5=int(input("enter your number"))  # isse input number me convert ho jata he
+# print(type(var5))
+
+
+# var6=float(input("enter your number"))  # isse input float me convert ho jata he
+# print(type(var6))
+
+
+# type casting
+
+num2=10
+change=str(num2)
+print(type(change))
+
+# array=["hellow","my","name","is",name,24]
+
+str2="python is a langauge of backend"
+print(str2.upper())
+print(str2.lower())
+print(len(str2))   # this is for length
+print(str2[0:2])   # this is for slice
+print(Fname+" "+Lname) # this is for concat
+
+
+var6="""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+
+'Tis your bright and tiny spark,
+Lights the trav'ller in the dark:
+Tho' I know not what you are,
+Twinkle, twinkle, little star."""
+
+
+print(var6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
